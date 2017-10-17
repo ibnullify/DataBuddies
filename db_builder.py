926afa399b3f1aa1b07360d698352666dd140143
anish2454 <shenoy.anish.227@gmail.com>
2017-10-17 19:09:17 -0400

    Fixed some errors

--- db_builder.py
+++ db_builder.py
@@ -15,14 +15,10 @@ os.remove(f) #Used During Testing to remove file at the beginning
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
-
-
-
 names_to_grades = {}
 ids_to_names = {}
 students_to_ids = {}
 
-    
 def populate():
     input_file = csv.DictReader(open("peeps.csv"))#Reads CSV
     c.execute("CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER)")
@@ -76,17 +72,17 @@ def make_ids_to_names_dict():
 Takes in a student name and a names_to_grade dict
 Returns the grades of the student
 '''
-def get_grades(student, names_to_grades):
+def get_grades(student):
     return names_to_grades[student]
 
 '''
 Takes in a student and a names_to_grade dict
 Returns the average of that student
 '''
-def calculate_avg(student, names_to_grades):
+def calculate_avg(student):
     sum = 0
     count = 0
-    grade_dict = get_grades(student, names_to_grades)
+    grade_dict = get_grades(student)
     for grade in grade_dict.values():
         sum += grade
         count += 1
@@ -110,12 +106,12 @@ def make_students_to_ids_dict():
            dict[name] = name_id[1] #Create a new dict for them and place their first grade
    #print(dict)
    return dict
-    
+
 '''
 prints the combination of the name, id, and average of a student
-'''    
-def display_name_id_avg(student,students_to_ids,names_to_grades):
-    avg = calculate_avg(student, names_to_grades)
+'''
+def display_name_id_avg(student):
+    avg = calculate_avg(student)
     #print students_to_ids["TOKiMONSTA"]
     id = students_to_ids[student]
     #print student + ", " + id + ", " + avg
@@ -127,27 +123,27 @@ def make_averages_table():
     c.execute(command)
     print ids_to_names
     for id in ids_to_names:
-        command = "INSERT INTO peeps_avg VALUES( " + str(id) + ", " + str(calculate_avg(ids_to_names[id], names_to_grades)) + ")"
+        command = "INSERT INTO peeps_avg VALUES( " + str(id) + ", " + str(calculate_avg(ids_to_names[id])) + ")"
         c.execute(command)
 
 
 def add_courses(code , mark , id):
-    command = "INSERT INTO courses VALUES (" +code + ", " + str(mark) +  ", " + str(id) + ")"
-    c.execute
+    command = "INSERT INTO courses VALUES (\"" + code + "\", " + str(mark) +  ", " + str(id) + ")"
+    c.execute(command)
 
-            
+
 populate()
 names_to_grades = make_names_to_grades_dict()
 ids_to_names = make_ids_to_names_dict()
 students_to_ids = make_students_to_ids_dict()
 print ids_to_names
 
-#print calculate_avg("TOKiMONSTA", names_to_grades)
-#print calculate_avg("sasha", names_to_grades)
-#display_name_id_avg("TOKiMONSTA",students_to_ids,names_to_grades)
-#display_name_id_avg("sasha",students_to_ids,names_to_grades)
+print calculate_avg("TOKiMONSTA")
+print calculate_avg("sasha")
+display_name_id_avg("TOKiMONSTA")
+display_name_id_avg("sasha")
 for name in students_to_ids:
-    display_name_id_avg(name, students_to_ids, names_to_grades)
+    display_name_id_avg(name)
 make_averages_table()
 add_courses("systems", 100, 2)
 db.commit() #save changes
